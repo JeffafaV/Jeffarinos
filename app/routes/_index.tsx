369a2d67a5eca21ec517254b443c20490b7be506
JeffafaV <47927752+JeffafaV@users.noreply.github.com>
2024-11-11 14:50:19 -0800

    Completed basic desktop layout of the main page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,5 +11,60 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return <div></div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-row items-center">
+        <div className="flex flex-col gap-2 text-center my-10">
+          <p className="font-bold text-7xl">YOU PROVIDE THE MONEYS</p>
+          <p className="font-bold text-7xl bg-red-600 text-white">
+            WE PROVIDE THE SHMEATS
+          </p>
+        </div>
+        <img
+          className="w-[600px] h-[600px]"
+          src="/images/whole_pizza.png"
+          alt="Whole Pizza"
+        ></img>
+      </div>
+
+      <div className="flex flex-row border-2 border-red-600 mx-auto w-fit gap-6 rounded-xl p-3 shadow-xl">
+        <span className="text-5xl border-r p-1">Start Here</span>
+        <button className="text-5xl bg-red-600 text-white rounded-xl p-1">
+          Menu
+        </button>
+      </div>
+
+      {/* featured heading */}
+      <div className="flex flex-row items-end gap-4 mt-5 mb-3">
+        <span className="text-6xl">Featured</span>
+        <div className="flex flex-row items-center">
+          <span className="text-red-600">SEE MORE</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </div>
+      </div>
+
+      {/* featured grid */}
+      <div className="flex flex-row gap-5 justify-center">
+        <div className="w-[350px] h-80 border border-red-600">
+          <p>hello</p>
+        </div>
+        <div className="w-[350px] h-80 border border-red-600"></div>
+        <div className="w-[350px] h-80 border border-red-600"></div>
+      </div>
+      <div></div>
+    </div>
+  );
 }
