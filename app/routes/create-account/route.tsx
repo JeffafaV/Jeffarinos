60b0448cba80db53974938e550d8be6a54b6f85e
Jeff Victorino <jeffyv914@gmail.com>
2025-02-10 17:31:11 -0800

    Added an icon inside the input boxes

--- app/routes/create-account/route.tsx
+++ app/routes/create-account/route.tsx
@@ -13,20 +13,17 @@ export async function action({ request }: ActionFunctionArgs) {
   const bcrypt = await import("bcrypt");
   const saltRounds = 10;
 
-  bcrypt.genSalt(saltRounds, (err: any, salt: string) => {
-    if (err) {
-      return;
-    }
+  const tempHash = await bcrypt.hash(password.toString(), saltRounds);
 
-    bcrypt.hash(password.toString(), salt, (err: any, hash: string) => {
-      if (err) {
-        return;
-      }
+  const match = await bcrypt.compare(password.toString(), tempHash);
 
-      console.log("Hashed password", hash);
-      console.log("Password", password);
-    });
-  });
+  if (match) {
+    console.log("Hash matches password");
+  } else {
+    console.log("What did I do wrong?");
+  }
+
+  // TODO put the hash in mongoDB database
   return null;
 }
 
@@ -41,56 +38,141 @@ const CreateAccount = () => {
           >
             First Name
           </label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            className="border rounded-md hover:border-red-700 focus:border-red-700 focus:m-[-1px] focus:border-2 focus:outline-none border-black peer"
-          />
+          <div className="flex flex-row border rounded-md hover:border-red-700 group-focus-within:border-red-700 group-focus-within:m-[-1px] group-focus-within:border-2 border-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 mr-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              className="w-full focus:outline-none border-transparent rounded-md"
+            />
+          </div>
         </div>
         <div className="flex flex-col group w-56">
           <label htmlFor="lastName" className="group-focus-within:text-red-700">
             Last Name
           </label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            className="border rounded-md hover:border-red-700 focus:border-red-700 focus:m-[-1px] focus:border-2 focus:outline-none border-black"
-          />
+          <div className="flex flex-row border rounded-md hover:border-red-700 group-focus-within:border-red-700 group-focus-within:m-[-1px] group-focus-within:border-2 border-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 mr-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              className="w-full focus:outline-none border-transparent rounded-md"
+            />
+          </div>
         </div>
         <div className="flex flex-col group w-56">
           <label htmlFor="username" className="group-focus-within:text-red-700">
             Username
           </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            className="border rounded-md hover:border-red-700 focus:border-red-700 focus:m-[-1px] focus:border-2 focus:outline-none border-black"
-          />
+          <div className="flex flex-row border rounded-md hover:border-red-700 group-focus-within:border-red-700 group-focus-within:m-[-1px] group-focus-within:border-2 border-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 mr-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="w-full focus:outline-none border-transparent rounded-md"
+            />
+          </div>
         </div>
         <div className="flex flex-col group w-56">
           <label htmlFor="email" className="group-focus-within:text-red-700">
             Email
           </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="border rounded-md hover:border-red-700 focus:border-red-700 focus:m-[-1px] focus:border-2 focus:outline-none border-black"
-          />
+          <div className="flex flex-row border rounded-md hover:border-red-700 group-focus-within:border-red-700 group-focus-within:m-[-1px] group-focus-within:border-2 border-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 mr-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full focus:outline-none border-transparent rounded-md"
+            />
+          </div>
         </div>
         <div className="flex flex-col group w-56">
           <label htmlFor="password" className="group-focus-within:text-red-700">
             Password
           </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="border rounded-md hover:border-red-700 focus:border-red-700 focus:m-[-1px] focus:border-2 focus:outline-none border-black"
-          />
+          <div className="flex flex-row border rounded-md hover:border-red-700 group-focus-within:border-red-700 group-focus-within:m-[-1px] group-focus-within:border-2 border-black">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 mr-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full focus:outline-none border-transparent rounded-md"
+            />
+          </div>
         </div>
 
         <p className="w-56 text-xs">
@@ -99,7 +181,7 @@ const CreateAccount = () => {
 
         <button
           type="submit"
-          className="border rounded-lg border-black p-1 hover:border-red-700"
+          className="border rounded-lg border-black p-1 pb-[6px] hover:border-red-700"
         >
           Submit
         </button>
